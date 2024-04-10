24476c10e7e960d7074e65ddeff6c4ca4dce5e23
DmitriyL <dmleybel@gmail.com>
2024-04-10 15:09:08 -0700

    added gold paragraphs example to kg0

--- from.cmd
+++ from.cmd
@@ -1,2 +1,2 @@
-docker cp e981e44f8d4d:/workspaces/AIIGS/cross_test.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/cross_test.ipynb
-docker cp e981e44f8d4d:/workspaces/AIIGS/musique/musique/musique_eda.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/musique_eda.ipynb
+docker cp zen_panini:/workspaces/AIIGS/cross_test.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/cross_test.ipynb
+docker cp zen_panini:/workspaces/AIIGS/musique/musique/musique_eda.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/musique_eda.ipynb
--- to.cmd
+++ to.cmd
@@ -1,1 +1,2 @@
-docker cp  C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/cross_test.ipynb e981e44f8d4d:/workspaces/AIIGS/cross_test.ipynb
+docker cp  C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/cross_test.ipynb zen_panini:/workspaces/AIIGS/cross_test.ipynb
+docker cp C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/musique_eda.ipynb zen_panini:/workspaces/AIIGS/musique/musique/musique_eda.ipynb 
