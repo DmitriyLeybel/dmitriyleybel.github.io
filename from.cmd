5a7e9e119700c57aea68caa3c937338c3c983e1f
DmitriyL <dmleybel@gmail.com>
2024-04-18 22:27:39 -0700

    History Management checkpint

--- from.cmd
+++ from.cmd
@@ -1,2 +1,3 @@
 docker cp zen_panini:/workspaces/AIIGS/cross_test.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/cross_test.ipynb
 docker cp zen_panini:/workspaces/AIIGS/musique/musique/musique_eda.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/musique_eda.ipynb
+docker cp zen_panini:/workspaces/AIIGS/musique_benchmark/kg_build.ipynb C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io\technical_blog\knowledge-graph-rag-benchmark-1\notebooks\kg_build.ipynb
--- to.cmd
+++ to.cmd
@@ -1,2 +1,3 @@
 docker cp  C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/cross_test.ipynb zen_panini:/workspaces/AIIGS/cross_test.ipynb
 docker cp C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io/notebooks/musique_eda.ipynb zen_panini:/workspaces/AIIGS/musique/musique/musique_eda.ipynb 
+docker cp C:\Users\dmley\OneDrive\Desktop\Sites\dmitriyleybel.github.io\technical_blog\knowledge-graph-rag-benchmark-1\notebooks\kg_build.ipynb zen_panini:/workspaces/AIIGS/musique_benchmark/kg_build.ipynb
